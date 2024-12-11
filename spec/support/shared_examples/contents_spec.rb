f3fc79a4931f4273632b887d0ad0ac19006f15c1
Popopond <pimpakan2545@gmail.com>
2024-12-11 10:58:44 +0700

    test

--- spec/support/shared_examples/contents_spec.rb
+++ spec/support/shared_examples/contents_spec.rb
@@ -1,9 +1,8 @@
 RSpec.shared_exaples "Contents" do |root_path|
   # Reordered code for better understanding of what is happening
 
-  scenario "User views all contents" do
-    visit_contents_page
-    expect_contents_to_be_displayed
+  scenario "USer create a new content" do
+    
   end
 
   scenario "User views all contents" do
